84f41c85fc44e17304d7842fe84aa3660be0dac9
rohan843 <rohan.sharma2934@gmail.com>
2025-04-25 18:17:03 +0530

    Add comments specifying byte size for quick lookup.
    Don't bother with the commit message prefixes like `fix:` or `doc:`

--- types.h
+++ types.h
@@ -1,16 +1,16 @@
 #ifndef __TYPES_H
 #define __TYPES_H
 
-typedef char int8_t;
-typedef unsigned char uint8_t;
+typedef char int8_t;           /*1 byte data*/
+typedef unsigned char uint8_t; /*1 byte unsigned data*/
 
-typedef short int16_t;
-typedef unsigned short uint16_t;
+typedef short int16_t;           /*2 byte data*/
+typedef unsigned short uint16_t; /*2 byte unsigned data*/
 
-typedef int int32_t;
-typedef unsigned int uint32_t;
+typedef int int32_t;           /*4 byte data*/
+typedef unsigned int uint32_t; /*4 byte unsigned data*/
 
-typedef long long int64_t;
-typedef unsigned long long uint64_t;
+typedef long long int64_t;           /*8 byte data*/
+typedef unsigned long long uint64_t; /*8 byte unsigned data*/
 
 #endif
