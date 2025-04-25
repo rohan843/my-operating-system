7cc338204935ff84ae0a9482dd50ef4874d0a128
rohan843 <rohan.sharma2934@gmail.com>
2025-04-25 17:25:17 +0530

    Init

---
+++ kernel.cpp
@@ -0,0 +1,28 @@
+#include "types.h"
+
+void printf(char *str)
+{
+    uint16_t *VideoMemory = (uint16_t *)0xb8000;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for (constructor *c = &start_ctors; c != &end_ctors; c++)
+    {
+        (*c)();
+    }
+}
+
+extern "C" void kernelMain(const void *multiboot_structure, uint32_t magicnumber)
+{
+    printf("Code runs like a flowing stream, Bits and bytes weave the dream, Logic dances, circuits gleam, Kernel wakes in a silent scream. ~ Copilot");
+    while (1)
+        ;
+}
---
+++ step-by-step-to-build-os.md
@@ -0,0 +1,4 @@
+# The Step by Step Guide to Building an Operating System
+
+1. Create basic files - `kernel.cpp`, `linker.ld`, `loader.s`, `Makefile`.
+2. Setup a way to create 
