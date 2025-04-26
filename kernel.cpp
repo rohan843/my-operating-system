78aa2aaebf12f0b071a687470e836eab953afddc
rohan843 <rohan.sharma2934@gmail.com>
2025-04-26 23:51:25 +0530

    Refactor printf and kernelMain documentaation for clarity and consistency

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,15 @@
 #include "types.h"
 #include "gdt.h"
 
+/**
+ * @brief Prints a string to the display.
+ * 
+ * This function places the charecters of a provided string directly into the VGA Video Memory
+ * location (0xb8000). Repetitive invocations of this function will lead to display being
+ *  overwritten.
+ * 
+ * @param str Pointer to the string being printed.
+ */
 void printf(char *str)
 {
     uint16_t *VideoMemory = (uint16_t *)0xb8000;
@@ -10,6 +19,9 @@ void printf(char *str)
     }
 }
 
+/**
+ * This piece of code runs all the C++ constructors.
+ */
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
@@ -21,6 +33,9 @@ extern "C" void callConstructors()
     }
 }
 
+/**
+ * @brief The main kernel function. This is the entry point into the OS program.
+ */
 extern "C" void kernelMain(const void *multiboot_structure, uint32_t magicnumber)
 {
     printf("Code runs like a flowing stream, Bits and bytes weave the dream, Logic dances, circuits gleam, Kernel wakes in a silent scream. ~ Copilot");
