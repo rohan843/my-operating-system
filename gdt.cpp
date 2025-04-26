a40a1b9b4b6faba60c89458a326f7b766acc09b8
rohan843 <rohan.sharma2934@gmail.com>
2025-04-26 22:47:41 +0530

    Add more comments to describe limit and flag creation process for segment descriptor.

--- gdt.cpp
+++ gdt.cpp
@@ -65,7 +65,7 @@ GlobalDescriptorTable::SegmentDescriptor::SegmentDescriptor(uint32_t base,
          *
          * This means that, if limit fits within 16 bits, we'll assume the segment's limit is
          * specified in terms of bytes instead of 4 KiB blocks.
-         * 
+         *
          * The flag is set to 0100, meaning the granularity is 0 (the MSB) and the D/B flag is set
          * to 1. This means that any code in this memory segment should have access to 32 - bit
          * operations instead of 16 - bit operations, i.e., registers like EAX would be accessible
@@ -76,7 +76,18 @@ GlobalDescriptorTable::SegmentDescriptor::SegmentDescriptor(uint32_t base,
     else
     {
         /**
-         *
+         * Here is a design choice we implemented. The variable `limit` was a 32 bit variable.
+         * According to the way we designed this system, if it is larger than what would fit in 16
+         * bits, we'll assume the 12 least significant bits are all 1's, and the 20 most significant
+         * bits will then be the count of 4 KiB blocks we need.
+         * 
+         * The below if-else block simply checks if the lower 12 bits aren't actually all 1's, we
+         * reduce the 13th lower significant bit (i.e., bit 12 if LSB was bit 0) by 1. This means
+         * that we're reducing the size of this memory segment than what was asked, but are ensuring
+         * we stay compliant with the 4 KiB block way of looking at the 20 high significant bits.
+         * 
+         * Finally, note also that we left - shift `limit` by 12. This is (obviously) to ensure
+         * `limit` reflects the count of 4 KiB blocks needed, instead of bytes needed.
          */
         if ((limit & 0xFFF) != 0xFFF)
         {
@@ -87,9 +98,20 @@ GlobalDescriptorTable::SegmentDescriptor::SegmentDescriptor(uint32_t base,
             limit = limit >> 12;
         }
 
+        /**
+         * Here, we set the flag to 1100. This means the granularity flag and the D/B flag, both are
+         * set. We saw the function of D/B previously (in the if-block above). The granularity flag
+         * specifies that we're interested in 4 KiB blocks rather than bytes as a unit, when looking
+         * at the value of `limit`.
+         */
         target[6] = 0xC0;
     }
 
+    /**
+     * At this point, we're aware that `limit` is a 20 - bit number (at most), and that the flag
+     * bits are set. Now we're simply placing the value of `limit` into the segment descriptor, at
+     * the appropriate places.
+     */
     target[0] = (uint8_t)(limit & 0xFF);
     target[1] = (uint8_t)((limit >> 8) & 0xFF);
     target[6] |= (uint8_t)((limit >> 16) & 0x0F);
