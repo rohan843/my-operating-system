04b06c8400d39fb61bf636fe594616a7fc432dda
rohan843 <rohan.sharma2934@gmail.com>
2025-04-26 23:25:53 +0530

    add docs

--- gdt.h
+++ gdt.h
@@ -12,7 +12,7 @@ class GlobalDescriptorTable
 public:
     /**
      * Desribes how an 8 byte segment entry looks like in the GDT.
-     * 
+     *
      * Limit: The size of the segment. (20 bits in all.) If limit fits in 16 bits, will be treated
      * as a count of bytes, otherwise as count of 4 KiB blocks of memory in the segment. THIS VALUE
      * MIGHT NOT BE THE ACTUAL SIZE ALLOCATED TO THE SEGMENT. CHECK THE SIZE AFTER SEGMENT CREATION.
@@ -32,7 +32,15 @@ public:
 
     public:
         SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
+
+        /**
+         * Returns the value of the base pointer for the segment.
+         */
         uint32_t Base();
+
+        /**
+         * Returns the value of the limit for the segment.
+         */
         uint32_t Limit();
     } __attribute__((packed));
 
