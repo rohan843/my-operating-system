c1dfb709338174488f811ccc9c693b3a62e67171
rohan843 <rohan.sharma2934@gmail.com>
2025-04-27 15:57:10 +0530

    Add code for improving printf working.

--- kernel.cpp
+++ kernel.cpp
@@ -5,17 +5,65 @@
  * @brief Prints a string to the display.
  *
  * This function places the charecters of a provided string directly into the VGA Video Memory
- * location (0xb8000). Repetitive invocations of this function will lead to display being
- *  overwritten.
+ * location (0xb8000). Fills the screen top to down, left to right. If the entire screen is filled,
+ * next characterss will be printed at the top of the screen, after clearing the screen.
  *
  * @param str Pointer to the string being printed.
  */
 void printf(char *str)
 {
+    /**
+     * @brief The memory location where the VGA display expects data to be kept.
+     *
+     * The dimensions of the character grid are 80 columns and 25 rows.
+     *
+     */
     uint16_t *VideoMemory = (uint16_t *)0xb8000;
+
+    /**
+     * @brief A cursor telling which row/column we're on at the moment.
+     */
+    static uint8_t x = 0, y = 0;
+
     for (int i = 0; str[i] != '\0'; i++)
     {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        if (str[i] != '\n')
+        {
+            VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+            x++;
+            if (x == 80)
+            {
+                /**
+                 * If the full row is filled, we go to the next line.
+                 */
+                y++;
+                x = 0;
+            }
+        }
+        else
+        {
+            /**
+             * If '\n' encountered, we go to the next line.
+             */
+            y++;
+        }
+        if (y == 25)
+        {
+            /**
+             * If all rows are filled, we clear the screen and go back to the top.
+             */
+
+            for (x = 0; x < 80; x++)
+            {
+                for (y = 0; y < 25; y++)
+                {
+                    VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | ' ';
+                }
+            }
+
+            x = 0;
+            y = 0;
+        }
     }
 }
 
@@ -38,12 +86,13 @@ extern "C" void callConstructors()
  */
 extern "C" void kernelMain(const void *multiboot_structure, uint32_t magicnumber)
 {
-    printf(
-        "Code runs like a flowing stream, "
-        "Bits and bytes weave the dream, "
-        "Logic dances, circuits gleam, "
-        "Kernel wakes in a silent scream. "
-        "~ Copilot");
+
+    printf("Code runs like a flowing stream,\n");
+    printf("Bits and bytes weave the dream,\n");
+    printf("Logic dances, circuits gleam,\n");
+    printf("Kernel wakes in a silent scream.\n");
+    printf("~ Copilot\n");
+    printf("Run #1\n");
 
     GlobalDescriptorTable gdt;
 
