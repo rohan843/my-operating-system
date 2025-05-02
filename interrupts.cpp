c5c3577d4f359cfd2e6bd8925867a6c32477a009
rohan843 <rohan.sharma2934@gmail.com>
2025-05-02 17:51:20 +0530

    Setup basic interrupt handling

--- interrupts.cpp
+++ interrupts.cpp
@@ -1,6 +1,10 @@
 #include "interrupts.h"
 
+void printf(char *str);
+
 uint32_t InterruptManager::handleInterrupt(uint8_t interruptNumber, uint32_t esp)
 {
+    printf(" Interrupt!");
+
     return esp; // for now.
 }
