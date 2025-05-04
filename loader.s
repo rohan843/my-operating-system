2ccabace91b34d6ffa4858e8c7d4dfe447fdd4d9
rohan843 <rohan.sharma2934@gmail.com>
2025-05-04 17:13:51 +0530

    Add comments around creation of kernel stack

--- loader.s
+++ loader.s
@@ -13,6 +13,7 @@
 .global loader
 
 loader:
+    # Initialize the stack pointer to the top of the kernel stack.
     mov $kernel_stack, %esp
 
     call callConstructors
@@ -26,6 +27,10 @@ _stop:
     hlt
     jmp _stop
 
+# The following bss section is used for the kernel stack. It specifies a 2 MiB space in memory, with
+# the label `kernel_stack` pointing to the top of the (currently empty) stack. As the stack grows,
+# the stack pointer (esp) will move towards lower memory addresses, i.e., it will be decremented
+# with each `push` instruction.
 .section .bss
 .space 2*1024*1024; # 2 MiB
 kernel_stack:
