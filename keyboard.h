f8729137fc374262e1b68bf9d75888a49fb4dd05
rohan843 <rohan.sharma2934@gmail.com>
2025-05-10 17:13:05 +0530

    Add doc explaining keyboard driver's overall operation

--- keyboard.h
+++ keyboard.h
@@ -1,3 +1,15 @@
+/**
+ * @file keyboard.h
+ * @author rohan843
+ * @brief Contains the driver for the keyboard.
+ * 
+ * This driver will setup the keyboard in such a manner that any byte sent to the data port of the
+ * keyboard will interrupt the CPU. It is the handler's responsibility to appropriately interpret
+ * the byte. (For e.g., the byte could be a key scan code, an acknowledgement from the keyboard,
+ * or something else.)
+ * 
+ */
+
 #ifndef __KEYBOARD_H
 #define __KEYBOARD_H
 
