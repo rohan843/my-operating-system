941e7790115c8a347593c0e8b52629218eb68fc7
rohan843 <rohan.sharma2934@gmail.com>
2025-05-10 23:50:15 +0530

    Improve key codes to character mapping

--- keyboard.cpp
+++ keyboard.cpp
@@ -54,106 +54,128 @@ KeyboardDriver::~KeyboardDriver()
 
 char us_qwerty_scancode_to_char(uint8_t scancode)
 {
+    static bool shift = false;
+    static bool capslock = false;
+
+    if (scancode == 0x2A || scancode == 0x36)
+    {
+        shift = true;
+        return '\0';
+    }
+    else if (scancode == 0xAA || scancode == 0xB6)
+    {
+        shift = false;
+        return '\0';
+    }
+
+    if (scancode == 0x3A)
+    {
+        capslock = !capslock;
+        return '\0';
+    }
+
+    bool uppercase = shift xor capslock;
+
     switch (scancode)
     {
     case 0x02:
-        return '1';
+        return shift ? '!' : '1';
     case 0x03:
-        return '2';
+        return shift ? '@' : '2';
     case 0x04:
-        return '3';
+        return shift ? '#' : '3';
     case 0x05:
-        return '4';
+        return shift ? '$' : '4';
     case 0x06:
-        return '5';
+        return shift ? '%' : '5';
     case 0x07:
-        return '6';
+        return shift ? '^' : '6';
     case 0x08:
-        return '7';
+        return shift ? '&' : '7';
     case 0x09:
-        return '8';
+        return shift ? '*' : '8';
     case 0x0A:
-        return '9';
+        return shift ? '(' : '9';
     case 0x0B:
-        return '0';
+        return shift ? ')' : '0';
     case 0x0C:
-        return '-';
+        return shift ? '_' : '-';
     case 0x0D:
-        return '=';
+        return shift ? '+' : '=';
     case 0x0E:
         return '\b'; // Backspace
     case 0x0F:
         return '\t'; // Tab
     case 0x10:
-        return 'q';
+        return uppercase ? 'Q' : 'q';
     case 0x11:
-        return 'w';
+        return uppercase ? 'W' : 'w';
     case 0x12:
-        return 'e';
+        return uppercase ? 'E' : 'e';
     case 0x13:
-        return 'r';
+        return uppercase ? 'R' : 'r';
     case 0x14:
-        return 't';
+        return uppercase ? 'T' : 't';
     case 0x15:
-        return 'y';
+        return uppercase ? 'Y' : 'y';
     case 0x16:
-        return 'u';
+        return uppercase ? 'U' : 'u';
     case 0x17:
-        return 'i';
+        return uppercase ? 'I' : 'i';
     case 0x18:
-        return 'o';
+        return uppercase ? 'O' : 'o';
     case 0x19:
-        return 'p';
+        return uppercase ? 'P' : 'p';
     case 0x1A:
-        return '[';
+        return shift ? '{' : '[';
     case 0x1B:
-        return ']';
+        return shift ? '}' : ']';
     case 0x1C:
         return '\n'; // Enter
     case 0x1E:
-        return 'a';
+        return uppercase ? 'A' : 'a';
     case 0x1F:
-        return 's';
+        return uppercase ? 'S' : 's';
     case 0x20:
-        return 'd';
+        return uppercase ? 'D' : 'd';
     case 0x21:
-        return 'f';
+        return uppercase ? 'F' : 'f';
     case 0x22:
-        return 'g';
+        return uppercase ? 'G' : 'g';
     case 0x23:
-        return 'h';
+        return uppercase ? 'H' : 'h';
     case 0x24:
-        return 'j';
+        return uppercase ? 'J' : 'j';
     case 0x25:
-        return 'k';
+        return uppercase ? 'K' : 'k';
     case 0x26:
-        return 'l';
+        return uppercase ? 'L' : 'l';
     case 0x27:
-        return ';';
+        return shift ? ':' : ';';
     case 0x28:
-        return '\'';
+        return shift ? '"' : '\'';
     case 0x29:
-        return '`';
+        return shift ? '~' : '`';
     case 0x2C:
-        return 'z';
+        return uppercase ? 'Z' : 'z';
     case 0x2D:
-        return 'x';
+        return uppercase ? 'X' : 'x';
     case 0x2E:
-        return 'c';
+        return uppercase ? 'C' : 'c';
     case 0x2F:
-        return 'v';
+        return uppercase ? 'V' : 'v';
     case 0x30:
-        return 'b';
+        return uppercase ? 'B' : 'b';
     case 0x31:
-        return 'n';
+        return uppercase ? 'N' : 'n';
     case 0x32:
-        return 'm';
+        return uppercase ? 'M' : 'm';
     case 0x33:
-        return ',';
+        return shift ? '<' : ',';
     case 0x34:
-        return '.';
+        return shift ? '>' : '.';
     case 0x35:
-        return '/';
+        return shift ? '?' : '/';
     case 0x39:
         return ' '; // Space
     default:
@@ -165,44 +187,36 @@ uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp)
 {
     uint8_t key = dataport.Read();
 
-    if (key < 0x80)
+    switch (key)
     {
-        /**
-         * Codes greater then 0x80 are key release codes. Codes less than 0x80 are key press codes.
-         *
-         * We're ignoring key releases for now.
-         *
-         */
-        switch (key)
+    /**
+     * Ignoring Num lock.
+     */
+    case 0x45:
+        break;
+    /**
+     * Ignoring ACK messages.
+     */
+    case 0xFA:
+        break;
+    default:
+        char result = us_qwerty_scancode_to_char(key);
+        if (result != '\0')
         {
-        /**
-         * Ignoring Num lock.
-         */
-        case 0x45:
-            break;
-        /**
-         * Ignoring ACK messages.
-         */
-        case 0xFA:
-            break;
-        default:
-            char result = us_qwerty_scancode_to_char(key);
-            if (result != '\0')
-            {
-                char *str = " ";
-                str[0] = result;
-                printf(str);
-            }
-            else
-            {
-                char *str = "\nKeyboard: 0x__";
-                char *hex = "0123456789ABCDEF";
-                str[13] = hex[(key >> 4) & 0xF];
-                str[14] = hex[key & 0xF];
-                printf(str);
-            }
-            break;
+            char *str = " ";
+            str[0] = result;
+            printf(str);
         }
+        // else
+        // {
+        //     char *str = "\nKeyboard: 0x__";
+        //     char *hex = "0123456789ABCDEF";
+        //     str[13] = hex[(key >> 4) & 0xF];
+        //     str[14] = hex[key & 0xF];
+        //     printf(str);
+        // }
+        break;
     }
+
     return esp;
 }
